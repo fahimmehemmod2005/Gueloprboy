850ed86a2b47af7d6c57427474a7a6f75204c506
fahimmehemmod2005 <mdfahimmehemmud@gmail.com>
2026-01-20 18:05:13 +0600

    home screen : widget title bar

--- lib/features/home/presentation/view/screens/home_screen.dart
+++ lib/features/home/presentation/view/screens/home_screen.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:gueloprboy/core/constant/app_color.dart';
-import 'package:gueloprboy/core/constant/app_images.dart';
-import 'package:gueloprboy/core/constant/app_text_styles.dart';
-
 import '../widgets/profile_list_tile.dart';
 import '../widgets/widget_background_image.dart';
 import '../widgets/widget_search_input_field.dart';
